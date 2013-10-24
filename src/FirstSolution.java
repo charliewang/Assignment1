bf7d955c4220e29dd03a0b77439be277284c21b7
charliewang <Qili@192.168.1.7>
2013-10-24 21:55:39 +1300

    Thursday PM work

---
+++ src/FirstSolution.java
@@ -0,0 +1,16 @@
+
+public class FirstSolution {
+	
+	public void FirstTry() {
+		
+		System.out.println("Please enter the date:");
+		String input = KeyboardInput.readInput();
+		
+		String inputProcess1 = input.trim();
+		
+		
+		System.out.println("You have entered the date: " + inputProcess1);
+		
+	}
+
+}
--- src/TheApp.java
+++ src/TheApp.java
@@ -3,8 +3,8 @@ public class TheApp {
 
 	public static void main(String[] args) {
 
-		IfEg m = new IfEg();
-		m.start();
+		FirstSolution x = new FirstSolution();
+		x.FirstTry();
 
 	}
 
